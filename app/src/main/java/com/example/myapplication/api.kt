4d61caae581762cbb9571046e39dccc4b2b69a64
nullchilly <nullchilly@gmail.com>
2023-03-06 07:40:56 +0700

    fix: replace com.sun.httpserver with java.net.URL

--- app/src/main/java/com/example/myapplication/api.kt
+++ app/src/main/java/com/example/myapplication/api.kt
@@ -1,4 +1,11 @@
 package com.example.myapplication
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class api {
+    // Create URL
+    var lichessEndpoint: URL = URL("https://lichess.org")
+
+    // Create connection
+    var myConnection: HttpsURLConnection = lichessEndpoint.openConnection() as HttpsURLConnection
 }
