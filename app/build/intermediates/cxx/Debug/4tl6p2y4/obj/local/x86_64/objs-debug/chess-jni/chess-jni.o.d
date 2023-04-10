331b1e16f37aa0001385f3cb5b0b857c7f937004
tunangoo <ngovantuan02092003@gmail.com>
2023-04-10 07:01:57 +0700

    chore: remove cached gitignore

--- app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/x86_64/objs-debug/chess-jni/chess-jni.o.d
+++ app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/x86_64/objs-debug/chess-jni/chess-jni.o.d
@@ -1,20 +1,20 @@
-/home/tako/code/git/android-chess/app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/x86_64/objs-debug/chess-jni/chess-jni.o: \
-  /home/tako/code/git/android-chess/native/project/jni/chess-jni.cpp \
-  /home/tako/code/git/android-chess/native/project/jni/chess-jni.hpp \
-  /home/tako/code/git/android-chess/native/project/jni/common.h \
-  /home/tako/code/git/android-chess/native/project/jni/ChessBoard.h \
-  /home/tako/code/git/android-chess/native/project/jni/Pos.h \
-  /home/tako/code/git/android-chess/native/project/jni/Move.h \
-  /home/tako/code/git/android-chess/native/project/jni/Game.h
+/home/tako/code/android/StockChess/app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/x86_64/objs-debug/chess-jni/chess-jni.o: \
+  /home/tako/code/android/StockChess/native/project/jni/chess-jni.cpp \
+  /home/tako/code/android/StockChess/native/project/jni/chess-jni.hpp \
+  /home/tako/code/android/StockChess/native/project/jni/common.h \
+  /home/tako/code/android/StockChess/native/project/jni/ChessBoard.h \
+  /home/tako/code/android/StockChess/native/project/jni/Pos.h \
+  /home/tako/code/android/StockChess/native/project/jni/Move.h \
+  /home/tako/code/android/StockChess/native/project/jni/Game.h
 
-/home/tako/code/git/android-chess/native/project/jni/chess-jni.hpp:
+/home/tako/code/android/StockChess/native/project/jni/chess-jni.hpp:
 
-/home/tako/code/git/android-chess/native/project/jni/common.h:
+/home/tako/code/android/StockChess/native/project/jni/common.h:
 
-/home/tako/code/git/android-chess/native/project/jni/ChessBoard.h:
+/home/tako/code/android/StockChess/native/project/jni/ChessBoard.h:
 
-/home/tako/code/git/android-chess/native/project/jni/Pos.h:
+/home/tako/code/android/StockChess/native/project/jni/Pos.h:
 
-/home/tako/code/git/android-chess/native/project/jni/Move.h:
+/home/tako/code/android/StockChess/native/project/jni/Move.h:
 
-/home/tako/code/git/android-chess/native/project/jni/Game.h:
+/home/tako/code/android/StockChess/native/project/jni/Game.h:
