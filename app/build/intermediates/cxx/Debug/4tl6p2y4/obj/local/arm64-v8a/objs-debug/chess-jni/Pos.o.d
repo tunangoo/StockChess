28184b20e54a42569752fd999a41a303b3f836c4
nullchilly <nullchilly@gmail.com>
2023-05-01 16:43:33 +0700

    fix: remove git ignored files

--- app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/arm64-v8a/objs-debug/chess-jni/Pos.o.d
+++
@@ -1,8 +0,0 @@
-/home/tako/code/git/android-chess/app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/arm64-v8a/objs-debug/chess-jni/Pos.o: \
-  /home/tako/code/git/android-chess/native/project/jni/Pos.cpp \
-  /home/tako/code/git/android-chess/native/project/jni/Pos.h \
-  /home/tako/code/git/android-chess/native/project/jni/common.h
-
-/home/tako/code/git/android-chess/native/project/jni/Pos.h:
-
-/home/tako/code/git/android-chess/native/project/jni/common.h:
--- app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/x86_64/objs-debug/chess-jni/Pos.o.d
+++
@@ -1,8 +0,0 @@
-/home/tako/code/android/StockChess/app/build/intermediates/cxx/Debug/4tl6p2y4/obj/local/x86_64/objs-debug/chess-jni/Pos.o: \
-  /home/tako/code/android/StockChess/native/project/jni/Pos.cpp \
-  /home/tako/code/android/StockChess/native/project/jni/Pos.h \
-  /home/tako/code/android/StockChess/native/project/jni/common.h
-
-/home/tako/code/android/StockChess/native/project/jni/Pos.h:
-
-/home/tako/code/android/StockChess/native/project/jni/common.h:
