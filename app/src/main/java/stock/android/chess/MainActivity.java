21edf401ae4b15c9679af3db8a6d1f82d3c19930
sownfam <ptson.0403@gmail.com>
2023-05-17 23:43:27 +0700

    refactor: start -> MainActivity

--- app/src/main/java/stock/android/chess/MainActivity.java
+++ app/src/main/java/stock/android/chess/MainActivity.java
@@ -1,11 +1,10 @@
 package stock.android.chess;
-
 import android.os.Bundle;
 
+import stock.android.chess.R;
 import stock.android.chess.activities.StartBaseActivity;
-import stock.android.chess.fragments.MainActivity;
 
-public class start extends StartBaseActivity {
+public class MainActivity extends StartBaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
